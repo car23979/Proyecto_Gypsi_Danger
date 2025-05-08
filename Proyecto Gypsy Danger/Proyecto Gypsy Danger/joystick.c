c0dbc6c00b765b5196dc84273aff9555f315f1b6
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-08 15:30:41 -0600

    Librerias para joystick

---
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/joystick.c
@@ -0,0 +1,6 @@
+/*
+ * joystick.c
+ *
+ * Created: 08/05/2025 03:30:04 p. m.
+ *  Author: Admin
+ */ 
---
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/joystick.h
@@ -0,0 +1,16 @@
+/*
+ * joystick.h
+ *
+ * Created: 08/05/2025 03:30:20 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef JOYSTICK_H_
+#define JOYSTICK_H_
+
+
+
+
+
+#endif /* JOYSTICK_H_ */
