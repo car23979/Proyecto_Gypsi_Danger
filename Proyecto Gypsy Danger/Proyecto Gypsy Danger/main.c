fe9f38e41810708390ac35a5c92a4c6df2316e32
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-08 00:28:28 -0600

    Creando archivos

---
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto Gypsy Danger.c
+ *
+ * Created: 08/05/2025 12:25:21 a. m.
+ * Author : David Carranza
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
