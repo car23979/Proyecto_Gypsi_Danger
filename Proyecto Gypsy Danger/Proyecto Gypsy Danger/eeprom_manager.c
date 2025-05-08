214d59f6e07c342857b7fa8140f1df6e5420fc2b
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-08 15:33:05 -0600

    Librerias para la EEPROM

---
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.c
@@ -0,0 +1,6 @@
+/*
+ * eeprom_manager.c
+ *
+ * Created: 08/05/2025 03:32:07 p. m.
+ *  Author: Admin
+ */ 
---
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.h
@@ -0,0 +1,16 @@
+/*
+ * eeprom_manager.h
+ *
+ * Created: 08/05/2025 03:31:50 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef EEPROM_MANAGER_H_
+#define EEPROM_MANAGER_H_
+
+
+
+
+
+#endif /* EEPROM_MANAGER_H_ */
