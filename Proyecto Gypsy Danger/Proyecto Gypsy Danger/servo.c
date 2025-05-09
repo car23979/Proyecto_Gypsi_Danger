4fae16826cc568e21aa7f8ef1db0a219a4240667
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 00:29:17 -0600

    Servo.C
    Se configuro el timer0 para los servos del hombro

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/servo.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/servo.c
@@ -4,3 +4,17 @@
  * Created: 08/05/2025 03:29:15 p. m.
  *  Author: Admin
  */ 
+
+#include "servo.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "pin_definitions.h"
+
+static volatile uint16_t servo_positions[NUM_SERVOS];
+
+void servo_init(){
+	// Configurar Timer0 para servos de hombro
+	TCCR0A = (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
+	TCCR0B = (1 << CS01) | (1 << CS00);	// Prescales 64
+	
+}
