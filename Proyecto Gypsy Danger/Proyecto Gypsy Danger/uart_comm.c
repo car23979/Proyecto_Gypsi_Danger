5be8f79db9ffbd8041644c351202086f8f0380f8
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 06:45:57 -0600

    Uart_init
    Se calcula el valor a cargar en el registro UBRR para configurar la velocidad de comunicación.
    La división entre 16 es porque el UART del AVR usa un divisor de reloj interno.
    El -1 ajusta el valor según el formato del registro.

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/uart_comm.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/uart_comm.c
@@ -4,3 +4,18 @@
  * Created: 08/05/2025 03:36:14 p. m.
  *  Author: Admin
  */ 
+
+#include "uart_comm.h"
+#include "servo.h"
+#include "eeprom_manager.h"
+#include <avr/io.h>
+#include <string.h>
+#include <util/delay.h>
+
+void uart_init(uint32_t baud_rate) {
+	uint16_t ubrr = F_CPU / 16 / baud_rate - 1;
+	UBRR0H = (uint8_t)(ubrr >> 8);
+	UBRR0L = (uint8_t)ubrr;
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8-bit data
+}
