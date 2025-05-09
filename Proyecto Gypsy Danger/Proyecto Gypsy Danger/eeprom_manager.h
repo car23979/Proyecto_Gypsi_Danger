9347e3e6d4322a6b783d5d535acc6d0cd6befa5c
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 01:19:22 -0600

    Varaibles para eeprom
    Se configuran las varaibles para la eeprom

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.h
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.h
@@ -9,8 +9,12 @@
 #ifndef EEPROM_MANAGER_H_
 #define EEPROM_MANAGER_H_
 
+#include <stdint.h>
+#define NUM_POSITIONS 4
 
-
-
+void eeprom_init();
+void eeprom_save_position(uint8_t pos_num);
+void eeprom_load_position(uint8_t pos_num);
+uint8_t eeprom_get_position_data(uint8_t pos_num, uint8_t servo_num);
 
 #endif /* EEPROM_MANAGER_H_ */
