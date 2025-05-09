c1f807ef9eb8f6fbcab1088b2a028097a841d488
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 06:50:53 -0600

    string
    Esta función envía una cadena de texto (string) por comunicación UART

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/uart_comm.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/uart_comm.c
@@ -24,3 +24,9 @@ void uart_send_byte(uint8_t data) {
 	while(!(UCSR0A & (1 << UDRE0)));
 	UDR0 = data;
 }
+
+void uart_send_string(const char* str) {
+	while (*str) {
+		uart_send_byte(*str++);
+	}
+}
