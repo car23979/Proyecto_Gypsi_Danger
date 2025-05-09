5a60faf2da58727161f3c19d06b85eeea5dc22a0
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 17:13:11 -0600

    Correciones de eprom

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.c
@@ -8,13 +8,14 @@
 #include "eeprom_manager.h"
 #include "servo.h"
 #include <avr/eeprom.h>
+#include <string.h> // Para memcpy si es necesario
 
 static uint8_t eeprom_data[NUM_POSITIONS][NUM_SERVOS];
 
 void eeprom_init() {
 	// Leer todas las posiciones al iniciar
-	for(uint8_t pos = 0; pos < NUM_POSITIONS; pos++){
-		for(uint8_t servo = 0; servo < NUM_SERVOS; servo++){
+	for(uint8_t pos = 0; pos < NUM_POSITIONS; pos++) {
+		for(uint8_t servo = 0; servo < NUM_SERVOS; servo++) {
 			eeprom_data[pos][servo] = eeprom_read_byte((uint8_t*)(pos * NUM_SERVOS + servo));
 		}
 	}
@@ -22,9 +23,10 @@ void eeprom_init() {
 
 void eeprom_save_position(uint8_t pos_num) {
 	if(pos_num >= NUM_POSITIONS) return;
-	for(uint8_t servo = 0, servo < NUM_SERVOS; servo++) {
+	
+	for(uint8_t servo = 0; servo < NUM_SERVOS; servo++) {
 		uint16_t pos = servo_get_position(servo);
-		eeprom_data[pos_num][servo] = (uint8_t)(pos >> 2); // Almacenar como byte
+		eeprom_data[pos_num][servo] = (uint8_t)(pos >> 2);
 		eeprom_write_byte((uint8_t*)(pos_num * NUM_SERVOS + servo), eeprom_data[pos_num][servo]);
 	}
 }
