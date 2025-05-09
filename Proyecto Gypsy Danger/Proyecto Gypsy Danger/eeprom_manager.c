3f7ee93d69eca40662c94790c6336f612e783a8e
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 01:25:51 -0600

    EEPROM
    Se configura la lectura de todas las posciones al iniciar

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/eeprom_manager.c
@@ -4,3 +4,18 @@
  * Created: 08/05/2025 03:32:07 p. m.
  *  Author: Admin
  */ 
+
+#include "eeprom_manager.h"
+#include "servo.h"
+#include <avr/eeprom.h>
+
+static uint8_t eeprom_data[NUM_POSITIONS][NUM_SERVOS];
+
+void eeprom_init() {
+	// Leer todas las posiciones al iniciar
+	for(uint8_t pos = 0; pos < NUM_POSITIONS; pos++){
+		for(uint8_t servo = 0; servo < NUM_SERVOS; servo++){
+			eeprom_data[pos][servo] = eeprom_read_byte((uint8_t*)(pos * NUM_SERVOS + servo));
+		}
+	}
+}
