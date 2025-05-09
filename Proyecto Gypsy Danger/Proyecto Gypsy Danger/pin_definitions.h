504616065f594ff9b02d158fe22d7f3a4a01cd5c
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 00:02:55 -0600

    Definir pines y canales
    Se difinieron los pines que se van usar para los servos y los canales ADC para los joystick

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/pin_definitions.h
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/pin_definitions.h
@@ -9,6 +9,16 @@
 #ifndef PIN_DEFINITIONS_H_
 #define PIN_DEFINITIONS_H_
 
+// Pines de servomotores
+#define SERVO_HEAD_H_PIN	PD5
+#define SERVO_HEAD_V_PIN	PD6
+#define SERVO_ARM_L_PIN		PB1
+#define	SERVO_ARM_R_PIN		PB2
+
+// Canales ADC para joysticks
+#define JOY_HEAD_H_ADC		0
+#define	JOY_HEAD_V_ADC		1
+#define	JOY_HEAD_L_ADC		2
 
 
 
