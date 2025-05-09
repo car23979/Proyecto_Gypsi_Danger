9afa681f6c88a081b5d3d1f67a149f3355559ba6
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-09 01:07:57 -0600

    ADMUX
    Se configura el AVcc como referencia y se habilita el ADEN (enable) lo cual configur aun prescaler de 128

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/joystick.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/joystick.c
@@ -10,3 +10,7 @@
 #include "pin_definitions.h"
 #include <avr/io.h>
 
+void joystick_init() {
+	ADMUX = (1 << REFS0); // AVcc como referencia
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable, prescaler 128
+}
