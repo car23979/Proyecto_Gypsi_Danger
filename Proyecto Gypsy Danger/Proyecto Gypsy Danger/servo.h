69d2e76ce646933e7aac1cb5c456fc2cc77bf890
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-12 17:07:31 -0600

    Servo
    Correcciones, ya no se usa Timer 0 ahora Timer2

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/servo.h
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/servo.h
@@ -12,13 +12,12 @@
 #include <stdint.h>
 
 // Inicialización de Timer1 y Timer2
-
 void servo_init();
-void servo_set_position(servo_channel_t channel, uint16_t position);
-uint16_t servo_get_position(servo_channel_t channel);
 
-// Declarar las funciones que se usan internamente
-void servo_update(servo_channel_t channel);
-void servo_update_all();
+// Mover un servo dado el pin físico (usa ADC para determinar posición)
+void servo_set_position(uint8_t pin, uint16_t adc_value);
+
+// Internamente usada por el software PWM para los servos de cabeza
+void servo_software_pwm_update(void);
 
 #endif /* SERVO_H_ */
