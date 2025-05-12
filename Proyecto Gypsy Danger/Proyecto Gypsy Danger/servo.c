1563dd242c01dac785743736e007c7873704f5cd
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-12 17:30:43 -0600

    Reescritura de timers

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/servo.c
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/servo.c
@@ -6,31 +6,19 @@
  */ 
 
 #include "servo.h"
-#include <avr/io.h>
 #include <avr/interrupt.h>
 #include "pin_definitions.h"
 
-static volatile uint16_t servo_positions[NUM_SERVOS];
+volatile uint16_t servo_head_h_pulse = 1000;  // en microsegundos
+volatile uint16_t servo_head_v_pulse = 1000;
 
 void servo_init(){
-	// Configurar Timer0 para servos de hombro
-	TCCR0A = (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
-	TCCR0B = (1 << CS01) | (1 << CS00);	// Prescales 64
+	// Timer1 para servos de brazo (PB1 y PB2)
+	 
+	TCCR1A = (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
+	TCCR1B = (1 << CS01) | (1 << CS00);	// Prescales 64
 	
-	// Configurar Timer1 para servos de brazos
-	TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM00);
-	TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescales 8, en modo Fast PWM ICR1
-	ICR1 = 39999; // Periodo de 20ms
 	
-	// Establecer pines como salida
-	DDRD |= (1 << SERVO_HEAD_H_PIN) | (1 << SERVO_HEAD_V_PIN);
-	DDRB |= (1 << SERVO_ARM_L_PIN) | (1 << SERVO_ARM_R_PIN);
-	
-	// Posiciones iniciales (centro)
-	for(uint8_t i = 0; i < NUM_SERVOS; i++) {
-		servo_positions[i] = 1500; // 1.5ms (posición central)
-	}
-	servo_update_all();
 }
 
 void servo_set_position(servo_channel_t channel, uint16_t position) {
