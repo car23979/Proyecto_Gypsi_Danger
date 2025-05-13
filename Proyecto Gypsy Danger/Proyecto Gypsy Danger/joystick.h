b2e402dc70d8a8b94fa660e6cb065ec011cebe39
car23979 <125841463+car23979@users.noreply.github.com>
2025-05-13 12:16:17 -0600

    Comentarios

--- Proyecto Gypsy Danger/Proyecto Gypsy Danger/joystick.h
+++ Proyecto Gypsy Danger/Proyecto Gypsy Danger/joystick.h
@@ -16,11 +16,16 @@ typedef enum {
 	JOY_HEAD_V,			// Canal 1 - Hombro derecho
 	JOY_ARM_L,			// Canal 2 - Brazo izquierdo
 	JOY_ARM_R,			// Canal 3 - Brazo derecho
-	NUM_JOYSTICKS
+	NUM_JOYSTICKS		// Número total de joysticks
 	} joystick_channel_t;
 
+// Inicializa Joysticks y el ADC
 void joystick_init();
+
+// Lee el valor de un canal específico del joystick
 uint16_t joystick_read(joystick_channel_t channel);
+
+// Actualiza las posiciones de los servos de acuerdo a los joysticks
 void joystick_update_servos();
 
 #endif /* JOYSTICK_H_ */
